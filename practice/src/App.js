fa5329fef6b0d71666fb1b27914f37b149f05bbb
poojashelke8 <poojabshelke0807@gmail.com>
2023-12-12 23:43:52 +0530

    started with new react app for practice

---
+++ practice/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UseState_p from './Components/UseState_p';
+
+function App() {
+  return (
+    <div className="App">
+      <UseState_p/>
+    </div>
+  );
+}
+
+export default App;
---
+++ practice/src/Components/UseState_p.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UseState_p = () => {
+  return (
+    <div>UseState_p</div>
+  )
+}
+
+export default UseState_p
